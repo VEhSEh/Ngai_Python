b6f388d1e882d7a5927d569e4be3ae06832e1f8b
VEhSEh <eruck.emmanuel@ubuea.cm>
2018-12-05 08:07:43 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import socket               # Import socket module
 import psycopg2             # import python postgres module
+import json
 
 #function to connect database
 def connectdb(database, user, password, host='127.0.0.1', port='5432'):
@@ -9,10 +10,11 @@ def connectdb(database, user, password, host='127.0.0.1', port='5432'):
 	return con
 
 #function to get results
-def returnDatabaseResults(sql, cursor):
+def returnDatabaseResults(sql, con):
+	cursor = con.cursor()
 	cursor.execute(sql)                    #executes the querry on the database
 	results = cursor.fetchall()            #fetch the results 
-	return results                         #return the results
+	return json.dumps(results)                         #return the results
 
 
 #conncect to the database
@@ -23,25 +25,26 @@ host = '127.0.0.1'
 port = '5432'
 con = connectdb(database,user,password, host,port)
 print ("database opened successfully")
+result = returnDatabaseResults('select * from patient_info', 
 
-#create a cursor handler
-cursor = con.cursor()
+# create a cursor handler
+# cursor = con.cursor()
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+# s = socket.socket()         # Create a socket object
+# host = socket.gethostname() # Get local machine name
+# port = 12345                # Reserve a port for your service.
+# s.bind((host, port))        # Bind to the port
 
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from', addr)
-   sql = c.recv(1024)   #receive statement to execute from client
-   results = returnDatabaseResults(sql, cursor)    #execute querry and return the results
-   for result in results:
-   	c.send(result[0])
-   	c.send(result[1])
-   	c.send(result[2])
-   	c.send(result[3])
-   #c.send('Thank you for connecting')
-   c.close()                # Close the connection
+# s.listen(5)                 # Now wait for client connection.
+# while True:
+#    c, addr = s.accept()     # Establish connection with client.
+#    print ('Got connection from', addr)
+#    sql = c.recv(1024)   #receive statement to execute from client
+#    results = returnDatabaseResults(sql, cursor)    #execute querry and return the results
+#    for result in results:
+#    	c.send(result[0])
+#    	c.send(result[1])
+#    	c.send(result[2])
+#    	c.send(result[3])
+#    #c.send('Thank you for connecting')
+#    c.close()                # Close the connection
